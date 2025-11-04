5be29738c4ed9f5b09c5dde8982c232f88eeadd0
Aditya Chaudhary <adityanshu45@gmail.com>
2025-11-04 13:05:44 +0530

    pricing componect code successfully

--- src/landing_page/pricing/Brokerage.jsx
+++ src/landing_page/pricing/Brokerage.jsx
@@ -2,7 +2,24 @@ import React from 'react';
 
 function Brokerage() {
     return (
-        <h1>Brokerage</h1>
+        <div className="container border-top brokerage">
+            <div className="row">
+                <div className="col-8 brokerage-content">
+                    <a href=""><h3>Brokerage calculator</h3></a>
+                    <ul>
+                        <li>Call & Trade and RMS auto-squareoff: Additional charges of 250+ GST per order.</li>
+                        <li>Digital contract notes will be sent via e-mail.</li>
+                        <li>Physical copies of contract notes, if required, shall be charged 220 per contract note. Courier charges apply.</li>
+                        <li>For NRI account (non-PIS), 0.5% or 100 per executed order for equity (whichever is lower).</li>
+                        <li>For NRI account (PIS), 0.5% or 2200 per executed order for equity (whichever is lower).</li>
+                        <li>If the account is in debit balance, any order placed will be charged 40 per executed order instead of 220 per executed order.</li>
+                    </ul>
+                </div>
+                <div className="col-4 brokerage-content">
+                    <a href=""><h3>List of charges</h3></a>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- src/landing_page/pricing/PricingPage.jsx
+++ src/landing_page/pricing/PricingPage.jsx
@@ -3,12 +3,13 @@ import Navbar from '../Navbar';
 import Hero from './Hero';
 import Brokerage from './Brokerage';
 import Footer from '../Footer';
-
+import OpenAccount from '../OpenAccount';
 
 function PricingPage() {
     return (
         <>
             <Hero />
+            <OpenAccount />
             <Brokerage />
         </>
     );
